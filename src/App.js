491414eebca73a6aa48c1e2fee425d12d8b05edf
Gabriel Anguiano <anguianog97@gmail.com>
2020-09-21 15:51:15 -0700

    added components. grid finally displaying. onclick will be done tommorow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import Grid from './components/Grid/Grid';
 
 function App() {
   return (
     <div className="App">
       <h1>Game Of Life</h1>
+      <Grid/>
     </div>
   );
 }
