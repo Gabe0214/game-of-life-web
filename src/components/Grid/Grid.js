041c845466f67ea0851be3b5531a263030ac738e
Gabriel Anguiano <anguianog97@gmail.com>
2020-09-22 15:58:59 -0700

    implemented a button that increases grid size. works perfectly

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,21 +1,19 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import produce from 'immer';
 
 
-const numRows = 30;
-const numCols = 40;
+// const numRows = 30;
+// const numCols = 40;
 
 const Grid = () => {
-    const [grid, setGrid] = useState(() => {
-        const rows = []
-        for (let i = 0; i < numRows; i++){
-          rows.push(Array.from(Array(numCols), () => 0))
-        }
-        return rows
-    })
-    console.log(grid)
+    const [numRows, setNumRows] = useState(30)
+    const [numCols, setNumCols] = useState(30)
+   
+    const [grid, setGrid] = useState(null)
+    const [simulation, setSimulation] = useState(false)
 
-   const clickedCell = (i,k) => {
+   
+    const clickedCell = (i,k) => {
        const newGrid = produce(grid, gridCopy => {
            gridCopy[i][k] = gridCopy[i][k] ? 0 : 1;
        })
@@ -30,13 +28,40 @@ const Grid = () => {
     }
      return setGrid(rows)
    }
+   
 
+   const runningRef = useRef(simulation)
+   runningRef.current = simulation
+   const runSimulation = useCallback(() => {
+      if(!runningRef.current){
+          return
+      }
+
+      setTimeout(runSimulation, 1000)
+   }, [])
+
+
+   useEffect(() => {
+    const rows = []
+    for (let i = 0; i < numRows; i++){
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+    return setGrid(rows)
+   }, [numCols, numRows])
+ 
+
+   const increaseGridSize = () => {
+      setNumCols(numCols + 10 )
+      setNumRows(numRows + 10)
+   }
 
     return(
-    <>
+    <>   
+        <button onClick={increaseGridSize}>+</button>
+        <button onClick={runSimulation}>{simulation ? 'Stop' : 'Start'}</button>
         <button onClick={clearGrid}>Clear</button>
         <div style={{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`, margin: '0 auto', justifyContent:'center', marginBottom:'5%'}}>
-            {grid.map((rows, i) => rows.map((col, k) => (
+            {grid && grid.map((rows, i) => rows.map((col, k) => (
                 <div key={`${i}-${k}`}
                 onClick={()=>clickedCell([i],[k])}
                 style={{height: 20, width: 20, backgroundColor: grid[i][k] ? "limegreen": null,
