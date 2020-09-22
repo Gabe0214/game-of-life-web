1d03895cedd7550a475175f546daa252fcde4ac3
Gabriel Anguiano <anguianog97@gmail.com>
2020-09-22 11:50:06 -0700

    cell toggle complete. also clear button functions properly

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import produce from 'immer';
+
 
 const numRows = 30;
 const numCols = 40;
@@ -12,10 +14,31 @@ const Grid = () => {
         return rows
     })
     console.log(grid)
+
+   const clickedCell = (i,k) => {
+       const newGrid = produce(grid, gridCopy => {
+           gridCopy[i][k] = gridCopy[i][k] ? 0 : 1;
+       })
+
+        setGrid(newGrid)
+   }
+
+   const clearGrid = () => {
+    const rows = []
+    for (let i = 0; i < numRows; i++){
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+     return setGrid(rows)
+   }
+
+
     return(
+    <>
+        <button onClick={clearGrid}>Clear</button>
         <div style={{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`, margin: '0 auto', justifyContent:'center', marginBottom:'5%'}}>
             {grid.map((rows, i) => rows.map((col, k) => (
                 <div key={`${i}-${k}`}
+                onClick={()=>clickedCell([i],[k])}
                 style={{height: 20, width: 20, backgroundColor: grid[i][k] ? "limegreen": null,
                 border: '1px solid grey'
             }}
@@ -23,6 +46,7 @@ const Grid = () => {
                 </div>
             )))}
         </div>
+    </>
     )
 }
 
